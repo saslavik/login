9fe76ef64cdb11caf8dae34055b6147ee6b0b360
saslavik <saslavik@gmail.com>
2021-01-07 13:25:17 +0200

    home work

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <header>
+        <div class="logo">
+          <img src="https://tocode.ru/static/img/general/logo.png" alt="toCode">
+        </div>
+        <div class="auth">
+          <span class="login" @click="login = true">
+            Login
+          </span>
+          <modalLogin v-show="login" @close="login = false" @change="changeModal()"/>
+          <!-- registration modal -->
+          <span class="login" @click="registration = true">
+            SignUp
+          </span>
+          <modalRegistration v-show="registration"
+          @close="registration = false"
+          @change="changeModal()" />
+        </div>
+      </header>
+      <section>
+
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import modalLogin from '@/components/ModalLogin.vue';
+import modalRegistration from '@/components/ModalRegistration.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    modalLogin,
+    modalRegistration,
+  },
+  data() {
+    return {
+      login: false,
+      registration: false,
+    };
+  },
+  methods: {
+    changeModal() {
+      this.login = !this.login;
+      this.registration = !this.registration;
+    },
   },
 };
 </script>
@@ -24,5 +59,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.wrapper {
+  align-items: center;
+}
+
+.wrapper-content {
+  width: 960px;
+  background-color: #fff;
+}
+
+header {
+  display: flex;
+  padding: 10px 0 !important;
+  justify-content: space-between;
+  border-bottom: 2px solid #f7f7f7;
+}
+
+.auth {
+  width: 120px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.login {
+  display: block;
+  line-height: 100%;
+  align-items: center;
+  cursor: pointer;
+}
+p.changeModal {
+  font-size: 12px !important;
+  margin: 20px 0 !important;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
+
+import '@/assets/scss/main.scss';
+
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render(h) { return h(App); },
+  render: (h) => h(App),
 }).$mount('#app');
